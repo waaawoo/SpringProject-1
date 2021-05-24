906562a44285ea69c0c92f7d27c26f0521b36c85
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-05-24 12:23:55 +0900

    正規表現でのバリデーションチェック

--- src/main/java/com/example/demo/ShainForm.java
+++ src/main/java/com/example/demo/ShainForm.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 public class ShainForm {
 //	notEmptyはから判定
 	@NotEmpty(message="社員Noを入れてください")
+//	正規表現でのバリデーション設定
+	@Pattern(regexp = "\\d{3}", message = "半角数字、3桁で入力してください。")
 //	formの値を宣言
 	private String number;
 	
