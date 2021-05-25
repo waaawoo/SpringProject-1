c23d93b98250bf268b24c41aef70dcba95f6710a
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-05-25 18:01:59 +0900

    サービス層のDI化実装

--- src/main/java/com/example/demo/ShainServiseInpl.java
+++ src/main/java/com/example/demo/ShainServiseInpl.java
@@ -1,12 +1,24 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Service;
+
+// サービス層のDI化に必要な記述　条件
+@Service
 public class ShainServiseInpl implements ShainServise {
+	
+//	使用するファイルの宣言
+	private final ShainRepository shainRepository;
+//	DI化
+	public ShainServiseInpl(ShainRepository shainRepository) {
+		this.shainRepository = shainRepository;
+	}
 
 //	オーバーライドする
 	@Override
 	public String findByNo(String Number) {
 //		リポジトリから社員を選択
-		ShainRepository shainRepository = new ShainRepositoryImpl();
+//		ShainRepository shainRepository = new ShainRepositoryImpl();
+//		DI化することでshainRepositoryが実装していなくても使用できる
 		String name = shainRepository.selectByNo(Number);
 		return name;
 	}
